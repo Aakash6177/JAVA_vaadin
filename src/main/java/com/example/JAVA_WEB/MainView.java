9dd9297d65468646089309e4d3617fd1f81303c7
Aakash6177 <49572566+Aakash6177@users.noreply.github.com>
2023-05-01 02:36:02 -0700

    Fixed some code

--- src/main/java/com/example/JAVA_WEB/MainView.java
+++ src/main/java/com/example/JAVA_WEB/MainView.java
@@ -1,22 +1,72 @@
 package com.example.JAVA_WEB;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.EmailField;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.data.binder.ValidationException;
 import com.vaadin.flow.router.Route;
+import org.hibernate.boot.jaxb.Origin;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.component.Component;
+import org.hibernate.boot.jaxb.spi.Binding;
 
-import java.awt.*;
+
 
 @Route("")
 public class MainView extends VerticalLayout {
 
-    public MainView(){
-        add(new H1("Hello World!"));
-        var btn = new Button("Click Me!");
-        var textField = new TextField();
+    private final PersonRepository repository;
+    private TextField first_name = new TextField("Fist Name");
+    private TextField last_name = new TextField("Last Name");
+    private EmailField email = new EmailField("Email");
 
-        add(textField,btn);
+    private Grid<Person> grid = new Grid<>(Person.class);
+    private Binder<Person> binder = new Binder<>(Person.class);
+
+
+    public MainView(PersonRepository repository){
+        this.repository = repository;
+
+        grid.setColumns("first_name","last_name","email");
+        add(getForm(), grid);
+        refreshGrid();
+    }
+
+    private Component getForm(){
+        var layout = new HorizontalLayout();
+
+        var addButton = new Button("Add ");
+        addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        layout.setAlignItems(Alignment.BASELINE);
+        layout.add(first_name, last_name, email, addButton);
+
+        binder.bindInstanceFields(this);
+
+        addButton.addClickListener(click ->{
+
+            try{
+                var person = new Person();
+                binder.writeBean(person);
+                repository.save(person);
+                refreshGrid();
+            }
+            catch(ValidationException e){
+
+            }
+        });
+
+        return layout;
+    }
+
+    private void refreshGrid(){
+        grid.setItems(repository.findAll());
     }
 
 }
